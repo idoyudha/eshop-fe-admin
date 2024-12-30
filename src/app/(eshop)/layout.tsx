615784f1838462ffc19caf20e9ebf9ac026b65ff
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2024-12-30 09:26:20 +0700

    refactor: move to eshop directory

---
+++ src/app/(eshop)/layout.tsx
@@ -0,0 +1,12 @@
+import "@/app/globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+export default async function EshopLayout({ children }: Readonly<{children: React.ReactNode}>) {
+    return (
+        <>
+            <main className="mx-auto flex w-full max-w-7xl flex-1 flex-col px-4 pb-6 pt-2 sm:px-6 lg:px-8">
+		    	{children}
+		    </main>
+        </>
+    )
+}
