6a15b5bcd1f73db14789e97fa280a3be3c9e6b00
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2024-12-30 10:17:21 +0700

    feat: auth components and page

---
+++ src/components/auth/signup-form.tsx
@@ -0,0 +1,115 @@
+"use client"
+
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Label } from "@radix-ui/react-label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+export function SignupForm({ className, ...props }: React.ComponentPropsWithoutRef<"div">) {
+    const [email, setEmail] = useState("")
+    const [username, setUsername] = useState("")
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const router = useRouter()
+
+    const handleSignUp = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            return;
+        }
+        try {
+            // await signup(email, username, name, password);
+            // toast.success('Successfully signed up, please check your email for verification code');
+        } catch (error) {
+            // toast.error('Signup failed. Please check your credentials and try again.');
+        }
+    };
+
+    return (
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl">Signup</CardTitle>
+                    <CardDescription>
+                        Fill you data
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={handleSignUp}>
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="m@eshop.com"
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="username">Username</Label>
+                                <Input
+                                    id="username"
+                                    type="username"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    placeholder="jumpscare789"
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="name">Name</Label>
+                                <Input
+                                    id="name"
+                                    type="name"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder="papa123"
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="password">Password</Label>
+                                <Input 
+                                    id="password" 
+                                    type="password" 
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required 
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="confirmPassword">Confirm Password</Label>
+                                <Input 
+                                    id="confirmPassword" 
+                                    type="password" 
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    required 
+                                />
+                            </div>
+                            <Button type="submit" className="w-full">
+                                Signup
+                            </Button>
+                            {/* <Button variant="outline" className="w-full" onClick={handleGoogleSignUp}>
+                                Login with Google
+                            </Button> */}
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
