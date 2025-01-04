c48f7c56b96870275864a2898743dbda88329e6e
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-04 11:08:41 +0700

    feat: add stock movement

--- src/app/(eshop)/stock-movement/page.tsx
+++ src/app/(eshop)/stock-movement/page.tsx
@@ -10,6 +10,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { getAllWarehouseProductsAction } from "@/actions/warehouse-product-actions";
 import { getAllStockMovementsAction } from "@/actions/stock-movement";
 import { ClientDataTable } from "./data-table-stock-movement-client";
+import { AddStockMoveInDialog } from "@/components/add-stock-movein-dialog";
 
 export default function StockMovementPage() {
     const { getAccessToken } = useAuth()
@@ -40,24 +41,27 @@ export default function StockMovementPage() {
 
     return (
         <>
-            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-                <SidebarTrigger className="-ml-1" />
-                <Separator orientation="vertical" className="mr-2 h-4" />
-                <Breadcrumb>
-                <BreadcrumbList>
-                    <BreadcrumbItem className="hidden md:block">
-                        <BreadcrumbLink href="#">
-                            Stock Movements
-                        </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator className="hidden md:block" />
-                    <BreadcrumbItem>
-                        <BreadcrumbPage>List</BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-                </Breadcrumb>
-            </div>
+            <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                <div className="flex items-center gap-2 px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem className="hidden md:block">
+                            <BreadcrumbLink href="#">
+                                Stock Movements
+                            </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator className="hidden md:block" />
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>List</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                    </Breadcrumb>
+                </div>
+                <div className="px-4">
+                    <AddStockMoveInDialog />
+                </div>
             </header>
             <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
                 <ClientDataTable stockMovements={stockMovements} />
