8ef01c0c52f6c205c626eb887b7983c44dcae43c
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-24 09:40:04 +0700

    feat: add badge on order page

--- src/app/(eshop)/order/columns.tsx
+++ src/app/(eshop)/order/columns.tsx
@@ -1,7 +1,10 @@
 "use client"
 
+import { Badge, badgeVariants } from "@/components/ui/badge";
+import { formatMoney } from "@/lib/utils";
 import { OrderView } from "@/models/order"
 import { ColumnDef } from "@tanstack/react-table"
+import { format } from "date-fns";
 
 export function useOrderColumns() {
     const columns: ColumnDef<OrderView>[] = [
@@ -12,18 +15,52 @@ export function useOrderColumns() {
         {
             accessorKey: "status",
             header: "Status",
+            cell: ({ row }) => {
+                const status = row.original.status;
+                switch (status) {
+                    case "PENDING":
+                    case "ON_DELIVERY":
+                        return <Badge variant="secondary">{status}</Badge>;
+                    case "PAYMENT_ACCEPTED":
+                    case "DELIVERED":
+                        return <Badge variant="outline">{status}</Badge>;
+                    default:
+                        return <Badge variant="destructive">{status}</Badge>;
+                }
+            }
         },
         {
             accessorKey: "total_price",
             header: "Total Price",
+            cell: ({ row }) => {
+                const totalPrice = row.original.total_price;
+                return formatMoney({ price: totalPrice, currency: "USD" });
+            }
         },
         {
             accessorKey: "payment_status",
             header: "Payment Status",
+            cell: ({ row }) => {
+                const paymentStatus = row.original.payment_status;
+                switch (paymentStatus) {
+                    case "PENDING":
+                        return <Badge variant="secondary">{paymentStatus}</Badge>;
+                    case "APPROVED":
+                        return <Badge>{paymentStatus}</Badge>;
+                    case "REJECTED":
+                        return <Badge variant="destructive">{paymentStatus}</Badge>;
+                    default:
+                        return <p>-</p>
+                }
+            }
         },
         {
             accessorKey: "created_at",
             header: "Created At",
+            cell: ({ row }) => {
+                const createdAt = row.original.created_at;
+                return format(new Date(createdAt), "dd MMM yyyy, hh:mm:ss a");
+            },
         }
     ]
 
